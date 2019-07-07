4adef3d5387018f7150603335a915a53bec4e410
ghst659 <gst659@gmail.com>
2019-07-06 19:20:47 -0700

    Added documentation and removed unnecessary iterator.

--- src/main/java/tc/brace/ParseNode.java
+++ src/main/java/tc/brace/ParseNode.java
@@ -5,6 +5,9 @@ import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.List;
 
+/**
+ * A node in the parse tree for a brace expansion pattern.
+ */
 public class ParseNode {
     /**
      * Parses the PATTERN into a collection of CharSequences,
@@ -18,7 +21,7 @@ public class ParseNode {
             Deque<StackNode> stack = new LinkedList<>();
             LinkedList<ParseNode> currentSequence = result;
             ParseNode currentChoice = null;
-            for (int i = 0; i < L; i++) {
+            for (int i = 0; i < L; ++i) {
                 Character ch = pattern.charAt(i);
                 switch (ch) {
                     case '{':
@@ -30,13 +33,12 @@ public class ParseNode {
                         currentChoice.addSequence(currentSequence);
                         break;
                     case '}':
-                        if (stack.size() > 0) {
-                            StackNode n = stack.pop();
-                            currentSequence = n.sequence;
-                            currentChoice = n.choice;
-                        } else {
+                        if (stack.size() < 0) {
                             throw new RuntimeException("invalid close-brace");
                         }
+                        StackNode n = stack.pop();
+                        currentSequence = n.getSequence();
+                        currentChoice = n.getChoice();
                         break;
                     case ',':
                         if (currentChoice == null) {
@@ -69,29 +71,45 @@ public class ParseNode {
         token.addChar(ch);
     }
 
+    /**
+     * Returns the choices represented by this ParseNode.
+     * @return a List of LinkedList of ParsxeNodes.
+     */
     public List<LinkedList<ParseNode>> getChoices() {
         return choices;
     }
 
+    /**
+     * Returns the atom value of this ParseNOde
+     * @return returns the atom StringBuffer.
+     */
     public StringBuffer getAtom() {
         return atom;
     }
-    private List<LinkedList<ParseNode>> choices = null;
-    private StringBuffer atom = null;
-    @Override
-    public String toString() {
-        return String.format("%s|%s", atom == null ? "-" : atom,
-                choices == null ? "-" : choices);
-    }
+
+    /**
+     * Tests if this ParseNode is an Atom and not a Choice.
+     * @return true if the ParseNode is an Atom.
+     */
     public boolean isAtom() {
         return choices == null;
     }
+
+    /**
+     * Adds a character to the atom value of this ParseNode.
+     * @param ch the character to add to the ParseNode.
+     */
     public void addChar(Character ch) {
         if (atom == null) {
             atom = new StringBuffer();
         }
         atom.append(ch);
     }
+
+    /**
+     * Adds a sequence to this PraseNode.
+     * @param seq the sequence to be added.
+     */
     public void addSequence(LinkedList<ParseNode> seq) {
         assert atom == null;
         if (choices == null) {
@@ -100,27 +118,15 @@ public class ParseNode {
         choices.add(seq);
     }
 
-    public ParseNodeVariations variations() {
-        return new ParseNodeVariations();
+    /**
+     * The string representation fo this ParseNode.
+     * @return the string representation of the ParseNode.
+     */
+    @Override
+    public String toString() {
+        return String.format("%s|%s", atom == null ? "-" : atom,
+                choices == null ? "-" : choices);
     }
-
-    private class ParseNodeVariations implements Iterator<String> {
-        @Override
-        public boolean hasNext() {
-            if (ParseNode.this.isAtom()) {
-                return true;
-            } else {
-                if (ParseNode.this.getChoices().size() > 0) {
-                    return false;
-                } else {
-                    return false;
-                }
-            }
-        }
-
-        @Override
-        public String next() {
-            return null;
-        }
-    }
+    private List<LinkedList<ParseNode>> choices = null;
+    private StringBuffer atom = null;
 }
--- src/main/java/tc/brace/StackNode.java
+++ src/main/java/tc/brace/StackNode.java
@@ -2,11 +2,25 @@ package tc.brace;
 
 import java.util.LinkedList;
 
+/**
+ * An item in the parsing stack.
+ */
 public class StackNode {
-    LinkedList<ParseNode> sequence = null;
-    ParseNode choice = null;
-    StackNode(LinkedList<ParseNode> seq, ParseNode alt) {
+    /**
+     * Constructof for the stack item
+     * @param seq a LinkedList of ParseNodes to be pushed on the stack.
+     * @param alt a ParseNode to be pushed on the stack.
+     */
+    public StackNode(LinkedList<ParseNode> seq, ParseNode alt) {
         sequence = seq;
         choice = alt;
     }
+    public ParseNode getChoice() {
+        return choice;
+    }
+    public LinkedList<ParseNode> getSequence() {
+        return sequence;
+    }
+    private LinkedList<ParseNode> sequence = null;
+    private ParseNode choice = null;
 }
